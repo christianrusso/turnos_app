6b73f1094cf275d7ec812c03f18272580bc9318c
leandrobono <leandrobono85@gmail.com>
2018-09-28 15:49:55 -0300

    Styles

--- src/pages/pre-search/pre-search.ts
+++ src/pages/pre-search/pre-search.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, AlertController, NavParams } from 'ionic-angular';
 import { LoginPage } from '../login/login';
+import { HttpClient } from '@angular/common/http';
+import { Constants } from '../../app/constants';
 
 @Component({
   selector: 'page-presearch',
@@ -9,13 +11,34 @@ import { LoginPage } from '../login/login';
 export class PreSearchPage {
 
   private category = "";
+  private categories;
 
   constructor(
       public navCtrl: NavController,
       public alertCtrl: AlertController,
-      public navParams: NavParams
+      public navParams: NavParams,
+      private http: HttpClient,
+      private constants: Constants
   ) {
     this.category = navParams.get("category");
+
+    let url = this.constants.API_URL + 'Data/GetCitiesForSelect';
+    let msj = '';
+    this.http.post(url, "").subscribe(
+        (success: any) => {
+          console.log(success);
+          this.categories = success;
+        },
+        error => {
+          console.log(error);
+          let alert = this.alertCtrl.create({
+            title: 'Error!',
+            subTitle: error.error,
+            buttons: ['OK']
+          });
+          alert.present();
+        }
+    );
   }
 
   goToLogin() {
