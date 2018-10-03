ccd33e2bb8b5ffcebdb89a8499834b6cba161193
leandrobono <leandrobono85@gmail.com>
2018-10-03 09:45:39 -0300

    Search styles

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Constants {
-    API_URL = 'http://orbitsa.xyz:5000/api/';
+    API_URL = 'http://45.55.35.40:5000/api/';
     constructor(
     ){
     }
