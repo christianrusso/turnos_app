e241fbb7f6d1edf9f9c561c69e48d130ca5398c1
leandrobono <leandrobono85@gmail.com>
2018-11-08 17:44:59 -0300

    Calendar

--- src/pages/turnos/turnos.ts
+++ src/pages/turnos/turnos.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { NavController, AlertController, NavParams } from 'ionic-angular';
+import { NavController, AlertController, NavParams, ModalController } from 'ionic-angular';
 import { InfoclinicaPage } from '../infoclinica/infoclinica';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import * as moment from 'moment';
 import { Constants } from '../../app/constants';
 import { UserService } from '../../services/user.service';
+import { CalendarModalOptions, DayConfig, CalendarModal } from "ion2-calendar";
 
 @Component({
   selector: 'page-turnos',
@@ -12,6 +14,11 @@ import { UserService } from '../../services/user.service';
 export class TurnosPage {
 
   public  results;
+  public  alertTurn = [];
+  private moment = moment();
+  public options: CalendarModalOptions = {
+    daysConfig: []
+  };
 
   constructor(
       public navCtrl: NavController,
@@ -19,7 +26,8 @@ export class TurnosPage {
       public navParams: NavParams,
       private http: HttpClient,
       private constants: Constants,
-      private userService: UserService
+      private userService: UserService,
+      private modalCtrl: ModalController
   ) {
   }
 
@@ -36,14 +44,40 @@ export class TurnosPage {
         'Authorization': 'Bearer ' + this.userService.getUserToken(),
       });
       let options = {
-        "startDate": "",
-        "endDate": "",
-        headers
+        "startDate": this.moment.format(moment.HTML5_FMT.DATETIME_LOCAL_SECONDS) + ".000Z",
+        "endDate": this.moment.add(30, 'days').format(moment.HTML5_FMT.DATETIME_LOCAL_SECONDS) + ".000Z"
       };
 
-      this.http.post(url, options).subscribe(
+      this.http.post(url, options, {headers}).subscribe(
           (success: any) => {
-            console.log(success);
+            let _daysConfig: DayConfig[] = [];
+            var clinics = success.clinic_GetWeekForClient;
+            var hairdress = success.hairdressing_GetWeekForClient;
+            clinics.forEach(element => {
+              if (element.appointments.length > 0) {
+                element.appointments.forEach(appoint => {
+                  _daysConfig.push({
+                    date: new Date(appoint.dateTime),
+                    marked: true,
+                    subTitle: element.appointments.length + " turnos",
+                    cssClass: 'Event'
+                  })
+                })
+              }
+            });
+            hairdress.forEach(element => {
+              if (element.appointments.length > 0) {
+                element.appointments.forEach(appoint => {
+                  _daysConfig.push({
+                    date: new Date(appoint.dateTime),
+                    marked: true,
+                    subTitle: element.appointments.length + " turnos",
+                    cssClass: 'Event'
+                  })
+                })
+              }
+            });
+            this.options.daysConfig = _daysConfig;
           },
           error => {
             console.log(error);
@@ -56,6 +90,61 @@ export class TurnosPage {
           }
       );
     }
+  }
+
+  showDayEvents(event) {
+    console.log(event);
+  }
+
+  searchSpecificDay(startDate, endDate) {
+    let url = this.constants.API_URL + "client/GetWeekForClient";
+    let headers = new HttpHeaders({
+      'Authorization': 'Bearer ' + this.userService.getUserToken(),
+    });
+    let options = {
+      "startDate": startDate + ".000Z",
+      "endDate": endDate + ".000Z"
+    };
+    this.http.post(url, options, {headers}).subscribe(
+        (success: any) => {
+          let data = [];
+          var clinics = success.clinic_GetWeekForClient;
+          var hairdress = success.hairdressing_GetWeekForClient;
+          clinics.forEach(element => {
+            if (element.appointments.length > 0) {
+              element.appointments.forEach(appoint => {
+                data.push({
+                  date: new Date(appoint.dateTime),
+                  marked: true,
+                  subTitle: element.appointments.length + " turnos",
+                  cssClass: 'Event'
+                })
+              })
+            }
+          });
+          hairdress.forEach(element => {
+            if (element.appointments.length > 0) {
+              element.appointments.forEach(appoint => {
+                data.push({
+                  date: new Date(appoint.dateTime),
+                  marked: true,
+                  subTitle: element.appointments.length + " turnos",
+                  cssClass: 'Event'
+                })
+              })
+            }
+          });
+        },
+        error => {
+          console.log(error);
+          let alert = this.alertCtrl.create({
+            title: 'Error!',
+            subTitle: error.error,
+            buttons: ['OK']
+          });
+          alert.present();
+        }
+    );
   }
 
 }
