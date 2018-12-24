8430767e3b513ae15d31d3d28d70ebb4e9e1319a
leandrobono <leandrobono85@gmail.com>
2018-12-24 11:20:59 -0300

    New URL

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Constants {
-    API_URL = 'https://orbitsa.xyz:4443/api/';
+    API_URL = 'https://todoreservas.com.ar:4443/api/';
     quantityOfResultsToShow = 10;
     constructor(
     ){
