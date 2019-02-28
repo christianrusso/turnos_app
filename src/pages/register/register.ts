5b66183082e88bee8ea8c171f754390b2d0cc420
leandrobono <leandrobono85@gmail.com>
2019-02-28 15:41:44 -0300

    Register validation

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -43,38 +43,49 @@ export class RegisterPage {
   }
 
   register() {
-    if (this.registerForm.value.clave == this.registerForm.value.reclave) {
-      let params = {email: this.registerForm.value.email,
-                    password: this.registerForm.value.clave,
-                    firstName: this.registerForm.value.nombre,
-                    lastName: this.registerForm.value.apellido};
-      let d = new Date().getMilliseconds();
-      let url = this.constants.API_URL + 'client/register';
-      let msj = '';
-      this.http.post(url, params).subscribe(
-          (success: any) => {
-            let alert = this.alertCtrl.create({
-              title: 'Error!',
-              subTitle: 'Registro Exitoso',
-              buttons: ['OK']
-            });
-            alert.present();
-            this.goToLogin();
-          },
-          error => {
-            console.log(error);
-            let alert = this.alertCtrl.create({
-              title: 'Error!',
-              subTitle: error.error,
-              buttons: ['OK']
-            });
-            alert.present();
-          }
-      );
+    if (this.registerForm.value.clave.length >= 6) {
+      if (this.registerForm.value.clave == this.registerForm.value.reclave) {
+        let params = {
+          email: this.registerForm.value.email,
+          password: this.registerForm.value.clave,
+          firstName: this.registerForm.value.nombre,
+          lastName: this.registerForm.value.apellido
+        };
+        let d = new Date().getMilliseconds();
+        let url = this.constants.API_URL + 'client/register';
+        let msj = '';
+        this.http.post(url, params).subscribe(
+            (success: any) => {
+              let alert = this.alertCtrl.create({
+                title: 'Error!',
+                subTitle: 'Registro Exitoso',
+                buttons: ['OK']
+              });
+              alert.present();
+              this.goToLogin();
+            },
+            error => {
+              console.log(error);
+              let alert = this.alertCtrl.create({
+                title: 'Error!',
+                subTitle: error.error,
+                buttons: ['OK']
+              });
+              alert.present();
+            }
+        );
+      } else {
+        let alert = this.alertCtrl.create({
+          title: 'Error!',
+          subTitle: "La repetición de la clave debe ser identica a la original",
+          buttons: ['OK']
+        });
+        alert.present();
+      }
     } else {
       let alert = this.alertCtrl.create({
         title: 'Error!',
-        subTitle: "La repetición de la clave debe ser identica a la original",
+        subTitle: "La contraseña debe contener al menos 6 caracteres",
         buttons: ['OK']
       });
       alert.present();
