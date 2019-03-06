57f74b225808edd2192ba6968df82524f5b72aa4
leandrobono <leandrobono85@gmail.com>
2019-03-06 14:55:29 -0300

    Validate dni

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Constants {
-    API_URL = 'https://todoreservas.com.ar:4443/api/';
-    //API_URL = 'http://localhost:5000/api/';
+    //API_URL = 'https://todoreservas.com.ar:4443/api/';
+    API_URL = 'http://localhost:5000/api/';
     quantityOfResultsToShow = 10;
     source = 3;
     baseUrlToSiteForShare = "http://todoreservas.com.ar/";
     clinicShareFolder = "infoclinica/";
     hairdressingShareFolder = "peluqueria";
+    dniLength = 7;
     constructor(
     ){
     }
--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -45,42 +45,51 @@ export class RegisterPage {
   }
 
   register() {
-    if (this.registerForm.value.clave.length >= 6) {
-      if (this.registerForm.value.clave == this.registerForm.value.reclave) {
-        let params = {
-          email: this.registerForm.value.email,
-          password: this.registerForm.value.clave,
-          firstName: this.registerForm.value.nombre,
-          lastName: this.registerForm.value.apellido,
-          dni: this.registerForm.value.dni
-        };
-        let d = new Date().getMilliseconds();
-        let url = this.constants.API_URL + 'client/register';
-        let msj = '';
-        this.http.post(url, params).subscribe(
-            (success: any) => {
-              let alert = this.alertCtrl.create({
-                title: 'Error!',
-                subTitle: 'Registro Exitoso',
-                buttons: ['OK']
-              });
-              alert.present();
-              this.goToLogin();
-            },
-            error => {
-              console.log(error);
-              let alert = this.alertCtrl.create({
-                title: 'Error!',
-                subTitle: error.error,
-                buttons: ['OK']
-              });
-              alert.present();
-            }
-        );
+    if (this.registerForm.value.dni.length >= this.constants.dniLength) {
+      if (this.registerForm.value.clave.length >= 6) {
+        if (this.registerForm.value.clave == this.registerForm.value.reclave) {
+          let params = {
+            email: this.registerForm.value.email,
+            password: this.registerForm.value.clave,
+            firstName: this.registerForm.value.nombre,
+            lastName: this.registerForm.value.apellido,
+            dni: this.registerForm.value.dni
+          };
+          let d = new Date().getMilliseconds();
+          let url = this.constants.API_URL + 'client/register';
+          let msj = '';
+          this.http.post(url, params).subscribe(
+              (success: any) => {
+                let alert = this.alertCtrl.create({
+                  title: 'Error!',
+                  subTitle: 'Registro Exitoso',
+                  buttons: ['OK']
+                });
+                alert.present();
+                this.goToLogin();
+              },
+              error => {
+                console.log(error);
+                let alert = this.alertCtrl.create({
+                  title: 'Error!',
+                  subTitle: error.error,
+                  buttons: ['OK']
+                });
+                alert.present();
+              }
+          );
+        } else {
+          let alert = this.alertCtrl.create({
+            title: 'Error!',
+            subTitle: "La repetición de la clave debe ser identica a la original",
+            buttons: ['OK']
+          });
+          alert.present();
+        }
       } else {
         let alert = this.alertCtrl.create({
           title: 'Error!',
-          subTitle: "La repetición de la clave debe ser identica a la original",
+          subTitle: "La contraseña debe contener al menos 6 caracteres",
           buttons: ['OK']
         });
         alert.present();
@@ -88,7 +97,7 @@ export class RegisterPage {
     } else {
       let alert = this.alertCtrl.create({
         title: 'Error!',
-        subTitle: "La contraseña debe contener al menos 6 caracteres",
+        subTitle: "El DNI debe tener un mínimo de 7 caracteres",
         buttons: ['OK']
       });
       alert.present();
