eac4ea3f12ad8fd6bdf93fb77eb30d5bac2c9893
Gabriel Fiocchi <gab@thinkapp.studio>
2019-04-12 00:19:00 +0200

    Update presearch design.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController, AlertController, Platform } from 'ionic-angular';
-import { FormBuilder } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
+import { NavController, AlertController } from 'ionic-angular';
 import { configs } from '../../app/constants';
-import { UserService } from '../../services/user.service';
 import { LoginPage } from '../login/login';
 import { PreSearchPage } from '../pre-search/pre-search';
 
@@ -12,14 +9,15 @@ import { PreSearchPage } from '../pre-search/pre-search';
   templateUrl: 'home.html'
 })
 export class HomePage implements OnInit {
-  pages;
+  pages: Array<{
+    name: string;
+    image: string;
+    route: string
+  }>;
+
   constructor(
-    private platform: Platform,
     public navCtrl: NavController,
-    private http: HttpClient,
     public alertCtrl: AlertController,
-    private formBuilder: FormBuilder,
-    private userService: UserService,
   ) { }
 
   ngOnInit() {
