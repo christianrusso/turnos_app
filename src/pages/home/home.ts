c0f977025a5dbd1955de9154148ffa65d22beaf7
Gabriel Fiocchi <gab@thinkapp.studio>
2019-04-11 22:32:10 +0200

    Update home styles.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController, AlertController, Platform } from 'ionic-angular';
-import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
-import { Constants } from '../../app/constants';
+import { configs } from '../../app/constants';
 import { UserService } from '../../services/user.service';
 import { LoginPage } from '../login/login';
 import { PreSearchPage } from '../pre-search/pre-search';
@@ -11,18 +11,20 @@ import { PreSearchPage } from '../pre-search/pre-search';
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
-
+export class HomePage implements OnInit {
+  pages;
   constructor(
     private platform: Platform,
     public navCtrl: NavController,
     private http: HttpClient,
     public alertCtrl: AlertController,
     private formBuilder: FormBuilder,
-    private constants: Constants,
     private userService: UserService,
   ) { }
 
+  ngOnInit() {
+    this.pages = configs.pages;
+  }
   goToLogin() {
     this.navCtrl.push(LoginPage);
   }
